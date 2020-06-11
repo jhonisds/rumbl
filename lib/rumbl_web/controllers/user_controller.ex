92e2b65ac751e8d5cfb10bceb1a0c586cd14628f
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-06-11 11:43:06 -0300

    create user view.

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -7,7 +7,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
 
   def index(conn, _params) do
-    IO.inspect(conn, label: "[User Controller]")
+    # IO.inspect(conn, label: "[User Controller]")
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
   end
