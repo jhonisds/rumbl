7d2ab0080c8495660c679c7730e373a5bfebe550
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-06-13 00:16:35 -0300

    create new user.

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,8 +4,7 @@ defmodule RumblWeb.UserController do
   """
   use RumblWeb, :controller
 
-  alias Rumbl.Accounts
-  alias Rumbl.Accounts.User
+  alias Rumbl.{Accounts, Accounts.User}
 
   def index(conn, _params) do
     # IO.inspect(conn, label: "[User Controller]")
@@ -20,8 +19,22 @@ defmodule RumblWeb.UserController do
   end
 
   def new(conn, _params) do
-    IO.inspect(self(), label: "[User Controller new]")
+    # IO.inspect(self(), label: "[User Controller new]")
     changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    # IO.inspect(user_params, label: "[User Controller create]")
+
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
