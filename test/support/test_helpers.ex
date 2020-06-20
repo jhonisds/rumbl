ae344854add77c9ec43d21aa815a4926db6cc898
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-06-20 10:30:20 -0300

    create user and video fixtures.

---
+++ test/support/test_helpers.ex
@@ -0,0 +1,28 @@
+defmodule Rumbl.TestHelpers do
+  alias Rumbl.{Accounts, Multimedia}
+
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "Some User",
+        username: "user #{System.unique_integer([:positive])}",
+        password: attrs[:password] || "supersecret"
+      })
+      |> Accounts.register_user()
+
+    user
+  end
+
+  def video_fixture(%Accounts.User{} = user, attrs \\ %{}) do
+    attrs
+    |> Enum.into(%{
+      title: "A Title",
+      url: "http://example.com",
+      password: "a description"
+    })
+
+    {:ok, video} = Multimedia.create_video(user, attrs)
+    video
+  end
+end
