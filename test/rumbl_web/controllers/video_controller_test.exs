1af7d3d399f917cf843bbf3d0aeb58a6bae1bf84
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-06-21 10:35:05 -0300

    add list all users video controller.

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -1,6 +1,13 @@
 defmodule RumblWeb.VideoControllerTest do
   use RumblWeb.ConnCase, async: true
 
+  setup %{conn: conn, login_as: username} do
+    user = user_fixture(uername: username)
+    conn = assign(conn, :current_user, user)
+
+    {:ok, conn: conn, user: user}
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each(
       [
@@ -17,5 +24,16 @@ defmodule RumblWeb.VideoControllerTest do
         assert conn.halted
       end
     )
+  end
+
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video = video_fixture(user, title: "funny cats")
+    other_video = video_fixture(user_fixture(username: "other"), title: "another video")
+
+    conn = get(conn, Routes.video_path(conn, :index))
+    assert html_response(conn, 200) =~ ~r/Listing Videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+
+    refute String.contains?(conn.resp_body, other_video.title)
   end
 end
